2e61cfe2e4fb0cfec28125f673af3efacb99e4b7
acharyna <nagaraj.acharya@gmail.com>
2019-12-17 00:38:19 +0530

    Added Zenefits API

--- orgchart/forms.py
+++ orgchart/forms.py
@@ -28,13 +28,39 @@ class ZenefitsForm(forms.Form):
 				self.company_id = result['data']['id']
 
 				# Save this new company to the model/database
-				new_company = Company(company_id=self.company_id, legal_name=result['data']['name'])
+				#new_company = Company(company_id=self.company_id, legal_name=result['data']['name'])
+				new_company = Company()
+				new_company.company_id = self.company_id
+				new_company.legal_name = result['data']['name']
 				new_company.save()
 
+				# Fetch all employees of this company using the Zenefits "People" API
+				# Ex: curl -i https://api.zenefits.com/core/people/?includes=company+manager+department+location -H "Authorization: Bearer XXXX"
 				result = self.get_request('https://api.zenefits.com/core/people/?includes=company+manager+department+location', token)
 				if result['success'] == True:
-					for person in result['data']:
-						print(person)
+					for person in result['data']['data']:
+						new_person = Person()
+						new_person.person_id = person['id']
+						new_person.company = new_company
+						new_person.status = person['status']
+						new_person.title = person['title']
+						new_person.first_name = person['first_name']
+						new_person.last_name = person['last_name']
+						if person['department'] is not None:
+							new_person.department_name = person['department']['name']
+						else:
+							new_person.department_name = None
+						if person['location'] is not None:
+							new_person.work_location = person['location']['city']
+						else:
+							new_person.work_location = None
+						new_person.work_email = person['work_email']
+						new_person.work_phone = person['work_phone']
+						if person['manager'] is not None:
+							new_person.manager_id = person['manager']['id']
+						else:
+							new_person.manager_id = None
+						new_person.save()
 
 #		request.session[tokens] = token # cache the token
 #		request.session[tokens][token] = company_id # cache the company_id
