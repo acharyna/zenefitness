014bf6cd56bd468e3ec8c25f38b14c51528fef88
acharyna <nagaraj.acharya@gmail.com>
2019-12-17 22:30:38 +0530

    Added caching

--- orgchart/admin.py
+++ orgchart/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Company, Person
+from .models import Company, Person, Cache
 
 admin.site.register(Company)
 admin.site.register(Person)
+admin.site.register(Cache)
 
